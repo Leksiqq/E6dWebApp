0550f1d904140c54286aa27a894b9f5f19e2bf6c
Alexey Zakharov <leksi@leksi.net>
2022-05-01 08:25:16 +0300

    Initial commit

---
+++ Library/Client.cs
@@ -0,0 +1,24 @@
+﻿namespace Net.Leksi.DocsRazorator;
+
+internal class Client
+{
+    private readonly HttpClient _client = new HttpClient();
+
+    internal Client(Uri baseAddress, string secretWord)
+    {
+        _client.BaseAddress = baseAddress;
+        _client.DefaultRequestHeaders.Add(DocsRazorator.SecretWordHeader, secretWord);
+    }
+    internal string Send(string request)
+    {
+        HttpRequestMessage requestMessage = new HttpRequestMessage(HttpMethod.Get, request);
+
+        HttpResponseMessage response = _client.Send(requestMessage);
+
+        if (response.IsSuccessStatusCode)
+        {
+            return response.Content.ReadAsStringAsync().Result;
+        }
+        throw new NotImplementedException(request);
+    }
+}
---
+++ Library/DocsRazorator.cs
@@ -0,0 +1,131 @@
+﻿using Microsoft.AspNetCore.Mvc.Razor.RuntimeCompilation;
+using Microsoft.Extensions.FileProviders;
+using System.Collections.Concurrent;
+using System.Reflection;
+
+namespace Net.Leksi.DocsRazorator;
+
+public class DocsRazorator
+{
+    internal const string SecretWordHeader = "X-Secret-Word";
+    public IEnumerable<string> Generate(IEnumerable<object> requisite, IEnumerable<string> requests)
+    {
+        ConcurrentQueue<string> queue = new();
+        ManualResetEventSlim manualReset = new();
+
+        manualReset.Reset();
+
+        Task loadTask = Task.Run(() =>
+        {
+            int port = 5000;
+            List<Assembly> assemblies = new();
+            List<object> requisitors = new();
+            foreach (object obj in requisite)
+            {
+                if (obj is Assembly asm)
+                {
+                    if (!assemblies.Contains(asm))
+                    {
+                        assemblies.Add(asm);
+                    }
+
+                }
+                else
+                {
+                    Assembly assembly = obj.GetType().Assembly;
+                    if (!assemblies.Contains(assembly))
+                    {
+                        assemblies.Add(assembly);
+                    }
+                    if (!requisitors.Contains(obj))
+                    {
+                        requisitors.Add(obj);
+                    }
+                }
+            }
+            while (true)
+            {
+                WebApplicationBuilder builder = WebApplication.CreateBuilder(new string[] { });
+
+                builder.Logging.ClearProviders();
+
+                builder.Services.AddRazorPages();
+
+                IMvcBuilder mvcBuilder = builder.Services.AddControllersWithViews();
+                foreach (Assembly assembly in assemblies)
+                {
+                    mvcBuilder.AddApplicationPart(assembly);
+                }
+                mvcBuilder.AddRazorRuntimeCompilation();
+
+                builder.Services.Configure<MvcRazorRuntimeCompilationOptions>(options =>
+                {
+                    foreach (Assembly assembly in assemblies)
+                    {
+                        options.FileProviders.Add(new EmbeddedFileProvider(assembly));
+                    }
+                });
+
+                foreach (object obj in requisitors)
+                {
+                    builder.Services.AddSingleton(obj.GetType(), op => obj);
+                }
+
+                WebApplication app = builder.Build();
+
+                string secretWord = Guid.NewGuid().ToString();
+
+                app.Use(async (context, next) =>
+                {
+                    if (!context.Request.Headers.ContainsKey(SecretWordHeader) || !context.Request.Headers[SecretWordHeader].Contains(secretWord))
+                    {
+                        context.Response.StatusCode = StatusCodes.Status403Forbidden;
+                        await context.Response.WriteAsync("");
+                    }
+                    else
+                    {
+                        await next.Invoke(context);
+                    }
+                });
+
+                app.MapRazorPages();
+
+                app.Lifetime.ApplicationStarted.Register(() =>
+                {
+                    Client client = new(new Uri(app.Urls.First()), secretWord);
+                    foreach (string request in requests)
+                    {
+                        queue.Enqueue(client.Send(request));
+                        manualReset.Set();
+                    }
+                    app.StopAsync();
+                });
+
+                app.Urls.Clear();
+                app.Urls.Add($"http://localhost:{port}");
+                try
+                {
+                    app.Run();
+                    break;
+                }
+                catch (IOException ex)
+                {
+                    Console.WriteLine(ex);
+                    ++port;
+                }
+            }
+        });
+
+        do
+        {
+            manualReset.Wait();
+            manualReset.Reset();
+            while (queue.TryDequeue(out string result))
+            {
+                yield return result;
+            }
+        }
+        while (!loadTask.IsCompleted);
+
+    }
+}
